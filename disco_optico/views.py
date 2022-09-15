ed3f5c82f1ec9b72cd434ca82f25b0f036e2c5fa
juanseabril <juanseabril@gmail.com>
2022-09-15 17:30:43 -0500

    Se carga la imagen del resultado del procesamiento al storage

--- disco_optico/views.py
+++ disco_optico/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 from rest_framework import status
 # from firebase_admin import credentials, initialize_app, storage
 import pyrebase
+import numpy as np
+import cv2
+import os
+import urllib
 
 class ImageViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin):
     queryset = Image.objects.all()
@@ -16,6 +20,7 @@ class ImageViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin):
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
+        # Configuracion firebase es la misma que en el front
         firebaseConfig = {
             "apiKey": "AIzaSyA8g3WsotiCGR4fN1J54YiKLfLmEQHCtEg",
             "authDomain": "lasalle-2f485.firebaseapp.com",
@@ -24,10 +29,26 @@ class ImageViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin):
             "storageBucket": "lasalle-2f485.appspot.com",
             "serviceAccount": "firebase/serviceAccountKey.json"
         }
-
         firebase_storage = pyrebase.initialize_app(firebaseConfig)
         storage = firebase_storage.storage()
 
-        storage.child("posts/35KCzS53lbfN541EdKWh/test").put("firebase/corazon.png")
+        # Lectura de imagen del storage
+        url = storage.child("posts/35KCzS53lbfN541EdKWh/image0_test.jpg").get_url(None)
+        req = urllib.request.urlopen(url)
+        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+        img = cv2.imdecode(arr, -1) # 'Load it as it is'
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        # Guarda en local la imagen temporalmente
+        cv2.imwrite('firebase/grises.png',gray_image)
+        cv2.imshow('gray', gray_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
+        # Carga de imagen al storage
+        storage.child("posts/35KCzS53lbfN541EdKWh/disco_optico_gray").put("firebase/corazon.png")
+        
+        # Elimina la imagen
+        os.remove("firebase/grises.png")
+        
         return Response(serializer.data['name'], status=status.HTTP_201_CREATED)
